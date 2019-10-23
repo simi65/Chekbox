9a9196c723d682216d11df4e12a05f1e6b268c77
MuAdMU <56901306+MuAdMU@users.noreply.github.com>
2019-10-23 10:07:38 +0700

    Update MainActivity.java

--- app/src/main/java/com/miss/chekbox/MainActivity.java
+++ app/src/main/java/com/miss/chekbox/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowEnabled(true);
 
         final CheckBox nasiGoreng = (CheckBox) findViewById(R.id.nasi_goreng);
         final CheckBox mieGoreng = (CheckBox) findViewById(R.id.mie_goreng);
